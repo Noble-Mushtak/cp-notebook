99556b84e27cb1cca0156a656aabdeb381f0c087
Noble-Mushtak <noblemushtak.public@gmail.com>
2022-03-03 13:12:50 -0500

    Added nealwu's hashmap

--- content/data-structures/HashMap.h
+++ content/data-structures/HashMap.h
@@ -1,28 +1,23 @@
 /**
- * Author: Simon Lindholm, chilli
- * Date: 2018-07-23
+ * Author: nealwu, Noble Mushtak
+ * Date: 2022-03-03
  * License: CC0
- * Source: http://codeforces.com/blog/entry/60737
- * Description: Hash map with mostly the same API as unordered\_map, but \tilde
- * 3x faster. Uses 1.5x memory.
- * Initial capacity must be a power of 2 (if provided).
+ * Source: https://codeforces.com/blog/entry/62393
+ * Description: Hash map with mostly the same API as unordered\_map, but safe against collision attacks
+ * Status: Tested against https://codeforces.com/contest/1039/problem/C
  */
 #pragma once
 
 #include <bits/extc++.h> /** keep-include */
-// To use most bits rather than just the lowest ones:
-struct chash { // large odd number for C
-	const uint64_t C = ll(4e18 * acos(0)) | 71;
-	ll operator()(ll x) const { return __builtin_bswap64(x*C); }
+const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+struct chash {
+	static uint64_t splitmix64(uint64_t x) {
+		x += 0x9e3779b97f4a7c15;
+		x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+		x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+		return x ^ (x >> 31);
+	}
+	size_t operator()(uint64_t x) const { return splitmix64(x + FIXED_RANDOM); }
 };
-__gnu_pbds::gp_hash_table<ll,int,chash> h({},{},{},{},{1<<16});
-
-/** For CodeForces, or other places where hacking might be a problem:
-
-const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash { // To use most bits rather than just the lowest ones:
-	const uint64_t C = ll(4e18 * acos(0)) | 71; // large odd number
-	ll operator()(ll x) const { return __builtin_bswap64((x^RANDOM)*C); }
-};
-__gnu_pbds::gp_hash_table<ll, int, chash> h({},{},{},{}, {1 << 16});
-*/
+template <class T> using hashset = __gnu_pbds::gp_hash_table<T, __gnu_pbds::null_type, chash>;
+template <class K, class V> using hashmap = __gnu_pbds::gp_hash_table<K, V, chash>;
